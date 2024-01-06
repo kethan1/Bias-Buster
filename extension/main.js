606d10f39ad8c77d42439c493d14377bc75b65a0
Kethan Vegunta <kethan@vegunta.com>
2024-01-05 21:38:20 -0800

    feat: initial commit

---
+++ extension/main.js
@@ -0,0 +1,6 @@
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+
+    document.getElementById("url").innerHTML = url;
+    // use `url` here inside the callback because it's asynchronous!
+});
