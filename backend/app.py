62961ce993472cf7471077788ea1d12f16d9d659
Kethan Vegunta <kethan@vegunta.com>
2024-01-06 00:07:55 -0800

    feat(backend): implement routes for get comments, post comment, and post rating

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,58 @@
+import os
+
 from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+mongo = PyMongo(app)
+db = mongo.dev
+CORS(app)
 
-@app.route("/api/v1")
-def home():
-    pass
+
+@app.route("/api/v1/get-comments")
+def get_comments():
+    url = request.args.get("url")
+    return db.comments.find({
+        "url": url
+    })
+
+
+@app.route("/api/v1/get-rating")
+def get_rating():
+    url = request.args.get("url")
+    return db.comments.aggregate([
+        {
+            "$match": {
+                "url": url
+            }
+        },
+        {
+            "$group": {
+                "_id": "$url",
+                "avgRating": {
+                    "$avg": "$rating"
+                }
+            }
+        }
+    ])
+
+
+@app.route("/api/v1/get-comments", methods=["POST"])
+def post_comment():
+    url = request.json["url"]
+    comment = request.json["comment"]
+    rating = request.json["rating"]
+    db.comments.insert_one({
+        "url": url,
+        "comment": comment,
+        "rating": rating
+    })
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
