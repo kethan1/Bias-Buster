eeec658b6b5aa1e2be5c17d304cae3c389423ab1
Kethan Vegunta <kethan@vegunta.com>
2024-01-06 13:38:16 -0800

    fix(extension): update extension ui

--- extension/main.js
+++ extension/main.js
@@ -1,73 +1,144 @@
-let url;
+const SERVER_URL = "http://localhost:5000";
+let url = "https://stackoverflow.com/questions/44192731/fetch-post-is-returning-http-415-while-curl-goes-on-fine-and-returns-result";
 
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    url = tabs[0].url;
+// chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+//   url = tabs[0].url;
 
-    Promise.all([
-        fetch("http://localhost:5000/api/v1/get-comments?"  + new URLSearchParams({
-            "url": url
-        })),
-        fetch("http://localhost:5000/api/v1/get-rating?"  + new URLSearchParams({
-            "url": url
-        }))
-    ]).then(responses => {
-        const [comments, rating] = responses;
-        Promise.all([comments.json(), rating.json()]).then(([comments, rating]) => {
-            // use both comments and rating here
-        });
+  const GET_SEARCH_PARAMS =
+    "?" +
+    new URLSearchParams({
+      url: url,
     });
-});
 
-addEventListener("load", () => {
-    for (let i=1; i<=5; i++) {
-        document.getElementById(i).addEventListener("click", () => rating(i));
+  Promise.all([
+    fetch(`${SERVER_URL}/api/v1/get-comments` + GET_SEARCH_PARAMS),
+    fetch(`${SERVER_URL}/api/v1/get-ratings` + GET_SEARCH_PARAMS),
+  ]).then((responses) => {
+    const [commentsResponse, ratingsResponse] = responses;
+    Promise.all([commentsResponse.json(), ratingsResponse.json()]).then(
+      ([comments, ratings]) => {
+        alert(ratings);
+        console.log(ratings);
+        let ratingNumbers = ratings.forEach(element => element.rating);
+        alert(`Mean: ${mean(rating)}\nMedian: ${median(rating)}\nMode: ${mode(rating)}`);
+      }
+    );
+  });
+// });
+
+function median(values) {
+  if (values.length === 0) return -1;
+
+  // Sorting values, preventing original array
+  // from being mutated.
+  values = [...values].sort((a, b) => a - b);
+
+  const half = Math.floor(values.length / 2);
+
+  return values.length % 2
+    ? values[half]
+    : (values[half - 1] + values[half]) / 2;
+}
+
+function mean(values) {
+    if (values.length === 0) return -1;
+
+    let sum = 0;
+    for (let i = 0; i < values.length; i++) {
+        sum += values[i];
     }
-});
+
+    return sum / values.length;
+}
+
+function mode(values) {
+    if (values.length === 0) return -1;
+
+    let count = {};
+    let max = 0;
+    let mode = -1;
+    for (let i = 0; i < values.length; i++) {
+        if (count[values[i]] === undefined) {
+            count[values[i]] = 1;
+        } else {
+            count[values[i]]++;
+        }
+
+        if (count[values[i]] > max) {
+            max = count[values[i]];
+            mode = values[i];
+        }
+    }
+
+    return mode;
+}
 
 function rating(starClicked) {
-    if (starClicked === 1) {
-        setStarColors(["red", "grey", "grey", "grey", "grey"]);
-    } else if (starClicked === 2) {
-        setStarColors(["orange", "orange", "grey", "grey", "grey"]);
-    } else if (starClicked === 3) {
-        setStarColors(["yellow", "yellow", "yellow", "grey", "grey"]);
-    } else if (starClicked === 4) {
-        setStarColors(["yellowgreen", "yellowgreen", "yellowgreen", "yellowgreen", "grey"]);
-    } else {
-        setStarColors(["green", "green", "green", "green", "green"]);
-    }
+  if (starClicked === 1) {
+    setStarColors(["red", "grey", "grey", "grey", "grey"]);
+  } else if (starClicked === 2) {
+    setStarColors(["orange", "orange", "grey", "grey", "grey"]);
+  } else if (starClicked === 3) {
+    setStarColors(["yellow", "yellow", "yellow", "grey", "grey"]);
+  } else if (starClicked === 4) {
+    setStarColors([
+      "yellowgreen",
+      "yellowgreen",
+      "yellowgreen",
+      "yellowgreen",
+      "grey",
+    ]);
+  } else {
+    setStarColors(["green", "green", "green", "green", "green"]);
+  }
 }
 
 function setStarColors(starColors) {
-    for (let i=1; i<=5; i++) {
-        document.getElementById(i).style.color = starColors[i-1];
-    }
+  for (let i = 1; i <= 5; i++) {
+    document.getElementById(i).style.color = starColors[i - 1];
+  }
 }
 
 function submitRating() {
-    let rating = getStarRating();
-    if (rating === -1) {
-        alert("Please select a rating");
-    } else {
-        fetch("http://localhost:5000/api/v1/create-post?"  + new URLSearchParams({
-            "url": url,
-            "rating": rating
-        })).then(response => {
-            if (response.ok) {
-                alert("Rating submitted");
-            } else {
-                alert("Rating failed");
-            }
-        });
-    }
+  let rating = getStarRating();
+  if (rating === -1) {
+    alert("Please select a rating");
+  } else {
+    fetch(`${SERVER_URL}/api/v1/add-rating`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        url: url,
+        rating: rating,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        alert("Rating submitted");
+      } else {
+        alert("Rating failed");
+      }
+    });
+  }
 }
 
 function getStarRating() {
-    let rating = 0;
-    for (let i=1; i<=5; i++) {
-        if (document.getElementById(i).style.color !== "grey") {
-            rating++;
-        }
+  let rating = 0;
+  for (let i = 1; i <= 5; i++) {
+    if (document.getElementById(i).style.color !== "grey") {
+      rating++;
     }
-    return rating > 0 ? rating : -1;
+  }
+  return rating > 0 ? rating : -1;
 }
+
+addEventListener("load", () => {
+  for (let i = 1; i <= 5; i++) {
+    document.getElementById(i).addEventListener("click", () => rating(i));
+  }
+  document
+    .getElementById("submitRating")
+    .addEventListener("click", submitRating);
+});
