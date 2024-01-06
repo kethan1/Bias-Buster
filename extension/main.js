0b332009e02b3b36af8d74717d6644b7d0f1ee3a
Kethan Vegunta <kethan@vegunta.com>
2024-01-06 12:44:50 -0800

    feat(extension): add new font and layout extension pop-up better

--- extension/main.js
+++ extension/main.js
@@ -1,46 +1,73 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
+let url;
 
-    document.getElementById("url").innerHTML = url;
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    url = tabs[0].url;
 
     Promise.all([
         fetch("http://localhost:5000/api/v1/get-comments?"  + new URLSearchParams({
             "url": url
         })),
-        fetch('/api/...')
-    ]).then(responses => {
-        const [comments, rating] = responses;
-    })
-
-    fetch("http://localhost:5000/api/v1/get-comments?"  + new URLSearchParams({
-        "url": url
-    })).then(comments => {
         fetch("http://localhost:5000/api/v1/get-rating?"  + new URLSearchParams({
             "url": url
-        })).then(rating => {
-            comments.json().then(comments => {
-                rating.json().then(rating => {
-                    // document.getElementById("rating").innerHTML = rating.rating;
-                    // document.getElementById("comments").innerHTML = comments.comments;
-                    // alert(rating);
-                    // alert(comments);
-                });
-            });
+        }))
+    ]).then(responses => {
+        const [comments, rating] = responses;
+        Promise.all([comments.json(), rating.json()]).then(([comments, rating]) => {
+            // use both comments and rating here
         });
-    });;
-    // use `url` here inside the callback because it's asynchronous!
+    });
 });
 
-function rating(starnum) {
-    if (starnum == 1) {
-        document
-    } else if (starnum == 2) {
-        
-    } else if (starnum == 3) {
-        
-    } else if (starnum == 4) {
-        
-    } else if (starnum == 5) {
-        
+addEventListener("load", () => {
+    for (let i=1; i<=5; i++) {
+        document.getElementById(i).addEventListener("click", () => rating(i));
     }
+});
+
+function rating(starClicked) {
+    if (starClicked === 1) {
+        setStarColors(["red", "grey", "grey", "grey", "grey"]);
+    } else if (starClicked === 2) {
+        setStarColors(["orange", "orange", "grey", "grey", "grey"]);
+    } else if (starClicked === 3) {
+        setStarColors(["yellow", "yellow", "yellow", "grey", "grey"]);
+    } else if (starClicked === 4) {
+        setStarColors(["yellowgreen", "yellowgreen", "yellowgreen", "yellowgreen", "grey"]);
+    } else {
+        setStarColors(["green", "green", "green", "green", "green"]);
+    }
+}
+
+function setStarColors(starColors) {
+    for (let i=1; i<=5; i++) {
+        document.getElementById(i).style.color = starColors[i-1];
+    }
+}
+
+function submitRating() {
+    let rating = getStarRating();
+    if (rating === -1) {
+        alert("Please select a rating");
+    } else {
+        fetch("http://localhost:5000/api/v1/create-post?"  + new URLSearchParams({
+            "url": url,
+            "rating": rating
+        })).then(response => {
+            if (response.ok) {
+                alert("Rating submitted");
+            } else {
+                alert("Rating failed");
+            }
+        });
+    }
+}
+
+function getStarRating() {
+    let rating = 0;
+    for (let i=1; i<=5; i++) {
+        if (document.getElementById(i).style.color !== "grey") {
+            rating++;
+        }
+    }
+    return rating > 0 ? rating : -1;
 }
