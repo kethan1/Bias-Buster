d754ede3c6c731f72e07ae84b32d97af6328b781
Kethan Vegunta <kethan@vegunta.com>
2024-01-07 11:23:06 -0800

    feat(extension): add flowbite

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     "./*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
